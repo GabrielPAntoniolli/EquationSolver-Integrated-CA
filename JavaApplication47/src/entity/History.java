98339743b91efe466bdbd11756ec2b4c42793c1b
natalia248 <73845447+natalia248@users.noreply.github.com>
2021-12-18 20:56:11 +0000

    adding entities

--- JavaApplication47/src/entity/History.java
+++ JavaApplication47/src/entity/History.java
@@ -7,8 +7,102 @@ package entity;
 
 /**
  *
- * @author gabri
+ * Entity class that mimics the database table "history", it has all the attributes that the original table has.
+ * This class is the one that handles the data taken from the database by creating the history object
  */
 public class History {
     
+    private Integer history_id;
+    private Integer user_id;
+    private String first_equation;
+    private String second_equation;
+    private String third_equation;
+    private Integer status;
+
+    public History(Integer history_id,Integer user_id, String first_equation,String second_equation,Integer status) {
+        this.history_id = history_id;
+        this.user_id = user_id;
+        this.first_equation = first_equation;
+        this.second_equation = second_equation;
+        this.status = status;
+    }
+    
+    public History(Integer history_id,Integer user_id, String first_equation,String second_equation,String third_equation ,Integer status) {
+        this.history_id = history_id;
+        this.user_id = user_id;
+        this.first_equation = first_equation;
+        this.second_equation = second_equation;
+        this.third_equation = third_equation;
+        this.status = status;
+    }
+    public History(Integer user_id, String first_equation,String second_equation,String third_equation ,Integer status) {
+        this.user_id = user_id;
+        this.first_equation = first_equation;
+        this.second_equation = second_equation;
+        this.third_equation = third_equation;
+        this.status = status;
+    }
+    public History(Integer user_id, String first_equation,String second_equation,Integer status) {
+        this.user_id = user_id;
+        this.first_equation = first_equation;
+        this.second_equation = second_equation;
+        this.third_equation = third_equation;
+        this.status = status;
+    }
+    
+
+    public Integer getHistory_id() {
+        return history_id;
+    }
+
+    public void setHistory_id(Integer history_id) {
+        this.history_id = history_id;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getFirst_equation() {
+        return first_equation;
+    }
+
+    public void setFirst_equation(String first_equation) {
+        this.first_equation = first_equation;
+    }
+
+    public String getSecond_equation() {
+        return second_equation;
+    }
+
+    public void setSecond_equation(String second_equation) {
+        this.second_equation = second_equation;
+    }
+
+    public String getThird_equation() {
+        return third_equation;
+    }
+
+    public void setThird_equation(String third_equation) {
+        this.third_equation = third_equation;
+    }
+
+    
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "History{" + "history_id=" + history_id + ", user_id=" + user_id + ", first_equation=" + first_equation + ", second_equation=" + second_equation + ", third_equation=" + third_equation + ", status=" + status + '}';
+    }
 }
--- JavaApplication47/src/entity/User.java
+++ JavaApplication47/src/entity/User.java
@@ -4,11 +4,93 @@
  * and open the template in the editor.
  */
 package entity;
-
-/**
+/*
  *
- * @author gabri
+ * Entity class that mimics the database table "user", it has all the attributes that the original table has.
+ * This class is the one that handles the data taken from the database by creating the user object
  */
-public class User {
+public class User{
+    
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+    private String username;
+    private String password;
+    private boolean isAdmin;
+    
+    public User(String name, String surname,int age,String username,String password, boolean isAdmin){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+    
+    public User(int id,String name, String surname, int age,String username,String password, boolean isAdmin){
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public int getId() {
+        return id;
+    }
+   
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name=" + name + ", surname=" + surname + ", age=" + age + ", username=" + username + ", password=" + password + ", isadmin=" + isAdmin + '}';
+    }
+    
+    
     
 }
